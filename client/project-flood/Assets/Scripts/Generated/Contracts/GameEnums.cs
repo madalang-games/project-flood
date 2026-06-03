49a0eb32668cd164502efe75f22bb95afeaf37c4
Sang <106306044+zzzz955@users.noreply.github.com>
2026-06-04 06:19:53 +0900

    chore#3: 생성 파일 및 Unity 설정 업데이트

---
+++ client/project-flood/Assets/Scripts/Generated/Contracts/GameEnums.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace ProjectFlood.Contracts.GameTypes
+{
+    public enum CellType
+    {
+        Basic    = 0x0,
+        Obstacle = 0x1,
+    }
+
+    public enum Difficulty
+    {
+        Easy   = 0,
+        Normal = 1,
+        Hard   = 2,
+    }
+}
